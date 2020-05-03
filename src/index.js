60c51c18802f7aed71d3ba839309933b01abe6e5
Dave Ceddia <dave@daveceddia.com>
2020-05-03 11:25:31 -0400

    Minimal example with CRA + Tailwind + tailwind config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './tailwind.generated.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = () => (
+ <div className="tw-max-w-md tw-mx-auto tw-flex tw-p-6 tw-bg-gray-100 tw-mt-10 tw-rounded-lg tw-shadow-xl">
+    <div className="tw-ml-6 tw-pt-1">
+      <h1 className="tw-text-2xl tw-text-blue-700 tw-leading-tight">
+        Tailwind and Create React App
+      </h1>
+      <p className="tw-text-base tw-text-gray-700 tw-leading-normal">
+        Building apps together
+      </p>
+    </div>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
